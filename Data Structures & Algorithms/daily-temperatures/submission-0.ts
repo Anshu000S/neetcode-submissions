634001b729fc8779c4f51d08a20ae25677597f37
Anshu000S <45619530+Anshu000S@users.noreply.github.com>
2026-05-27 21:44:42 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        let stack: Array<number> = [];
+        let result: Array<number> = new Array(temperatures.length).fill(0);
+
+        for (let i=0;i<temperatures.length;i++) {
+            const currentTemperature =  temperatures[i];
+            if (!stack.length) {
+                stack.push(i);
+                continue;
+            }
+
+            while (stack.length > 0 && currentTemperature > temperatures[stack[stack.length - 1]]) {
+                const poppedIndex = stack.pop()!;
+                result[poppedIndex] = i - poppedIndex;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
